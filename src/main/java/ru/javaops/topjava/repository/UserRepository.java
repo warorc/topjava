9de5d7aa72c1c6e5fa9b4c08b8ef7dee6ca03f3a
Vadim Polosov <warorc@Vadims-MacBook-Pro.local>
2023-05-04 11:32:46 +0700

    all 12 patches

---
+++ src/main/java/ru/javaops/topjava/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package ru.javaops.topjava.repository;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javaops.topjava.error.NotFoundException;
+import ru.javaops.topjava.model.User;
+
+import java.util.Optional;
+
+import static ru.javaops.topjava.config.SecurityConfig.PASSWORD_ENCODER;
+
+@Transactional(readOnly = true)
+public interface UserRepository extends BaseRepository<User> {
+
+    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    //    https://stackoverflow.com/a/46013654/548473
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.meals WHERE u.id=?1")
+    Optional<User> getWithMeals(int id);
+
+    @Transactional
+    default User prepareAndSave(User user) {
+        user.setPassword(PASSWORD_ENCODER.encode(user.getPassword()));
+        user.setEmail(user.getEmail().toLowerCase());
+        return save(user);
+    }
+
+    default User getExistedByEmail(String email) {
+        return findByEmailIgnoreCase(email).orElseThrow(() -> new NotFoundException("User with email=" + email + " not found"));
+    }
+}
